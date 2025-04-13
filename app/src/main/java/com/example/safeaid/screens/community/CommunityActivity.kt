035d20225a53895d61c258838b1d845838f8d68c
HNam <huydre37@gmail.com>
2025-04-13 10:33:41 +0700

    feat: add communtiy activiry

---
+++ app/src/main/java/com/example/safeaid/screens/community/CommunityActivity.kt
@@ -0,0 +1,21 @@
+package com.example.safeaid.screens.community
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidtraining.R
+
+class CommunityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_community)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
